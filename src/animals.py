eab73083990fe27a88e951f8413336684c541525
Shufan-Mao <46933875+Shufan-Mao@users.noreply.github.com>
2019-08-26 12:35:10 -0500

    Update animals.py

---
+++ src/animals.py
@@ -0,0 +1,8 @@
+class Animal:
+
+    def __init__(self):
+        self.id_number = None
+        self.category = None
+        self.size = None
+        self.location = None
+        self.status = None
