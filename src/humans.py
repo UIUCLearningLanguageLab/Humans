3844af875dee2ff156210fb5e97a55ec3d8686dd
Shufan-Mao <46933875+Shufan-Mao@users.noreply.github.com>
2019-08-26 12:31:36 -0500

    Update humans.py

--- src/humans.py
+++ src/humans.py
@@ -23,12 +23,13 @@ class Human:
         self.intelligence = None
 
     def take_turn(self):
-        x_delta = random.randint(-10, 10)
+        x_delta =  random.randint(-10, 10)
         y_delta = random.randint(-10, 10)
         if config.World.tile_size < self.x + x_delta < config.World.world_size-config.World.tile_size:
             self.x += x_delta
         if config.World.tile_size < self.y + y_delta < config.World.world_size-config.World.tile_size:
             self.y += y_delta
+            
 
     def get_hunt_success_probs(self, animal):
         best_action = None
@@ -39,28 +40,170 @@ class Human:
         trap_prob = 0
         return best_action, prob
 
-# given calculations of expectation about actions satisfying drives, choose an action
-'''
-        while tiredness < hunger:
-        
-            if food is stored:
-                go to the food
-                if the food is not butchered
-                    butcher the food
-                cook the food
-                eat the food
-                
+    def go_to(self,location):
+
+    def check_food(self,food_list):
+        food_amount = 0
+
+        return food_amount
+
+
+    def butcher(self,food):
+
+
+    def cook(self,food):
+
+    def eat(self,food):
+
+    def sleep(self):
+
+
+    # decide what to eat based on the extent of hunger and what is there in the fridge,
+    # choose food so that the energy contained in food should be no less than the sum of hunger and the energy consumed
+    # to conduct the cooking-eating sequence, if food is enough, return the food and amount, otherwise: if the energy
+    # is lower than threshold, return all food restored and amount, if energetic, return 0
+
+    def decide_food_to_eat(self):
+
+        food_decided = []
+        return food_decided
+
+    # go and look for animal to hunt, if nothing is found, return 0, else return one animal represented by
+    # (id, category, size, location)
+
+    def look_for_animal(self):
+        game = 0
+
+        return game
+
+
+    # calculate the expected energy gain from hunting the animal, where E(gain) = E(game)-E(killing)-E(carrying),
+    # E(game)= sum(energy of all animal found); E(carrying)= energy consumed to carry the animals;
+
+
+    def calculate_gain(self, game):
+        gain = 0
+        return gain
+
+
+
+    # decide whether to kill the found animal, based on the expected energy consumed to conduct the killing-carrying sequence
+    # against the gain, decide what animals to hunt so that satisfying the hunger return a list of Animal , otherwise, return 0.
+
+    def search(self):
+        game = self.look_for_animal()
+        if game:
+            game_value = self.calculate_gain(game)
+            if game_value > self.hunger:
+                animal_found = game
             else:
-                while animal not found
-                    search for animal
-                    
-                if "good chance of kill":
-                    pick appropriate method
-                    follow steps to trap, chase, 
+                animal_found = 0
+        else:
+            animal_found = game
+
+        return animal_found
+
+
+    def choose_killing_method(self, animal_found):
+
+
+    def kill(self, animal, method):
+
+
+    def carry_to_fridge(self, animal_found):
+
+    def get_up(self):
+
+
+
+
+    def take_action(self):
+        pass
+
+        # given calculations of expectation about actions satisfying drives, choose an action
+        if self.awake:
+            # when one is awake, he/she does three things: go sleeping, go get food, or just stay
+            # if sleepiness pass the threshold, then go sleeping, otherwise, if hunger pass some
+            # threshold, go get food, finally, if the neither sleepy nor hungry, then just stay.
+
+            if self.sleepiness >= self.sleep_threshold:
+                self.sleep()
+
+            else:
+                # fisrt go check how much food left, no matter hungry or not
+                self.go_to(Fridge)
+                food_amount = self.check_food(food_list)
+                if food_amount >= food_threshold:
+
+                    # in the case that food is enough(below the threshold), do cooking-eating sequence if hungry
+                    # do nothing if not
+
+                    if self.hunger < self.hunger_threshold:
+                        self.stay()
+                    else:
+                        to_eat_list = self.decide_food_to_eat()
+                        for animal in to_eat_list:
+                            if not animal.buthcered:
+                                self.butcher(animal)
+                            self.cook(animal)
+                            self.eat(animal)
+
+                else:
+                    # in the case that food is not enough, should go get food.
+
+                    # Yet, if hungry, should first get something to eat
+                    # get a full meal if enough food, otherwise, eat all stored food
+
+                    to_eat_list = self.decide_food_to_eat()
+                    if to_eat_list:
+                        for animal in to_eat_list:
+                            if not animal.buthcered:
+                                self.butcher(animal)
+                            self.cook(animal)
+                            self.eat(animal)
+
+                    # If not hungry, then go out and get food (hunt)
+                    else:
+                        energy_gain = 0
+                        hunt_goal = food_threshold - food_amount
+                        animal_killed = []
+                        animal_processed = []
+                        # do the search-go-kill-(butcher)-carry sequence until goal met
+                        while energy_gain < hunt_goal:
+                            animal_found = 0
+                            while animal_found == 0:
+                                animal_found = self.search()
+                            method = self.choose_killing_method(animal_found)
+                            animal = animal_found[1]
+                            animal_location = animal_found[3]
+                            self.go_to(animal_location)
+                            result = self.kill(animal, method)
+                            if result:
+                                animal_killed.append(animal)
+                                energy_gain = + animal[2]
+                                if animal[2] > self.carry_threshold:
+                                    butchered_animal = self.butcher(animal)
+                                    animal_processed.append(butchered_animal)
+                                else:
+                                    animal_processed.append(animal)
+                                self.carry_to_fridge(animal)
+
+                        # update the food storage
+                        for animal in animal_processed:
+                            food_list.append(animal)
+
+
+
+        else:
+            if self.sleepiness <= self.sleep_threshold:
+                self.get_up()
+
+
             
-'''
 # what is the high level goal (sleep, eat)
 # if sleep, do the sleep steps
 # if eat, is there gathered food available
+
+
 
 # within those, what are specific goal (eat
