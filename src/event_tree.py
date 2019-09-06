9b1c8227685927b23c5560e1b00be13cf1b752b5
Shufan-Mao <smao9@illinois.edu>
2019-09-06 14:10:26 -0500

    generating the event tree

--- src/event_tree.py
+++ src/event_tree.py
@@ -2,41 +2,52 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
 
-f = open('event_tree.txt')
-line_list = []
-for line in f:
-    text = line.strip('\n')
-    if len(text) > 0:
-        item = eval(text)
-        line_list.append(item)
-parallel = line_list[0]
-leaves = line_list[1:]
-tree = {}
-depth = 0
-T = nx.DiGraph()
-for leave in leaves:
-    code = leave[1]
-    if depth < len(code):
-        depth = len(code)
-    tree[code] = [leave[0],1]
-    l = len(code)
-    for i in range(l-1,-1,-1):
-        parent_code = code[:i]
-        child_code = code[:i+1]
-        if parent_code not in tree:
-            if parent_code in parallel:
-                tree[parent_code] = ['p',1]
+
+def initialize_event_tree(leave_file):
+    f = open(leave_file)
+    line_list = []
+    for line in f:
+        text = line.strip('\n')
+        if len(text) > 0:
+            item = eval(text)
+            line_list.append(item)
+    parallel = line_list[0]
+    leaves = line_list[1:]
+    tree = {}
+    depth = 0
+    T = nx.DiGraph()
+    for leave in leaves:
+        code = leave[1]
+        if depth < len(code):
+            depth = len(code)
+        tree[code] = [leave[0],1]
+        l = len(code)
+        for i in range(l-1,-1,-1):
+            parent_code = code[:i]
+            child_code = code[:i+1]
+            if parent_code not in tree:
+                if parent_code in parallel:
+                    tree[parent_code] = ['p',1]
+                else:
+                    tree[parent_code] = ['s',1]
+                T.add_edge(parent_code, child_code)
             else:
-                tree[parent_code] = ['s',1]
-            T.add_edge(parent_code, child_code)
-        else:
-            T.add_edge(parent_code, child_code)
-            break
+                T.add_edge(parent_code, child_code)
+                break
 
-for node in T:
-    if T.out_degree(node) > 0 and tree[node][0] == 's':
-        tree[node][1] = len([n for n in T.neighbors(node)])
+    for node in T:
+        if T.out_degree(node) > 0 and tree[node][0] == 's':
+            tree[node][1] = len([n for n in T.neighbors(node)])
 
-print(tree)
-nx.draw(T, with_labels=True)
-plt.show()
+    return tree, T
+
+
+def main():
+    leave_file = 'event_tree.txt'
+    tree, T = initialize_event_tree(leave_file)
+    print(tree)
+    pos=graphviz_layout(T, prog='dot')
+    nx.draw(T, pos, arrows=False, with_labels=False)
+    plt.show()
+
+main()
