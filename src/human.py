4303fe57d4c6a8864e0ad510d47bcb8b02a78e9f
Shufan-Mao <smao9@illinois.edu>
2019-09-11 16:56:45 -0500

    starting on simple event

--- src/human.py
+++ src/human.py
@@ -2,13 +2,15 @@ import random
 from src import config
 import operator
 import stats
+import csv
+import numpy as np
 from src import event_tree as et
 
 
 class Human:
 
     def __init__(self, world):
-
+        self.world = world
         self.x = random.randint(config.World.tile_size, config.World.world_size - config.World.tile_size)
         self.y = random.randint(config.World.tile_size, config.World.world_size - config.World.tile_size)
 
@@ -29,12 +31,11 @@ class Human:
         self.intelligence = None
         self.food_threshold = None
         self.carry_threshold = None
-
         self.sleep_threshold = None
         self.hunger_threshold = None
-
-        self.world = world
+        self.focus = None
         self.event_dict, self.event_tree = et.initialize_event_tree('event_tree.txt')
+        self.hunting_method = self.get_hunting_method()
 
         self.drive_dict = {'hunger': 0, 'sleepiness': 0}
         self.drive_values = [0, 0]
@@ -44,6 +45,37 @@ class Human:
 
         # action_dict = {'hunt_deer': [A, B, C, D]}
 
+    def get_hunting_method(self):
+        t = self.event_tree
+        hunting_skill, length = self.get_hunting_skill()
+        hunting_method = {}
+        method_list = [(0,0,0,1,0), (0,0,0,1,1), (0,0,0,1,2), (0,0,0,1,3)]
+        for method in method_list:
+            skills = [n for n in t.neighbors(method)]
+            success_rate = np.ones(length)
+            for skill in skills:
+                skill_name = self.event_dict[skill][0]
+                skill_rate = hunting_skill[skill_name]
+                success_rate = np.multiply(success_rate, skill_rate)
+            hunting_method[method] = success_rate
+        return hunting_method
+
+    def get_hunting_skill(self):
+        hunting_skill = {}
+        with open('random_sampling.csv') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            line = 0
+            for row in csv_reader:
+                if line == 0:
+                    length = len(row)-2
+                else:
+                    copy = []
+                    for item in row[1:-1]:
+                        copy.append(eval(item))
+                    hunting_skill[row[0]] = np.asarray(copy)
+                    line = line + 1
+        return hunting_skill, length
+
     def take_turn(self):
         t = self.event_tree
         self.compute_status()
@@ -51,9 +83,17 @@ class Human:
 
         if t.out_degree(self.current_event) == 0:  # currently on leave
             if status == 1:  # specific event functions to write
-                print(self.event_dict[self.current_event][0])
+                event_name = self.event_dict[self.current_event][0]
+                if event_name == 'search':
+                    self.search()
+                elif event_name == 'go_to':
+                    self.go_to()
+                elif event_name  in {'gather', 'butcher', 'cook', 'eat', 'lay_down', 'asleep', 'wake_up', 'get_up'}:
+                    self.do_it()
+                else:
+                    self.hunt()
             else:
-                self.current_event = self.current_event[len(self.current_event)-1]
+                self.current_event = self.current_event[:len(self.current_event)-1]
 
         elif t.in_degree(self.current_event) != 0:  # currently on branch
             if status == 0:
@@ -71,32 +111,37 @@ class Human:
         t = self.event_tree
         event_type = self.event_dict[self.current_event][0]
         children = [n for n in t.neighbors(self.current_event)]
-        num = len(children)
+        sorted_children = children.sort()
         if event_type == 's':
-            index = num - self.event_dict[self.current_event][1]
-            self.current_event = self.current_event + (index,)
-        else:  # choice function to write
-            scores = []
-            for child in children:
-                score = self.compute_scores(child)
-                scores.append(score)
-            self.current_event = self.make_decision(scores)
-        return self.current_event
-
-    def compute_scores(self,event):
-        if self.current_event == ():
-            score = 1
-        elif self.current_event == (0,0):
-            score = 2
-        elif self.current_event == (0,0,0,1):
-            score = 3
+            index = len(children) - self.event_dict[self.current_event][1]
+            self.current_event = sorted_children[index]
         else:
-            score = 4
-        return score
-
-    def make_decision(self,event,scores):
+            score = -float('Inf')
+            event = ''
+            for child in children:
+                new_score = self.compute_scores(child)
+                if new_score > score:
+                    score = new_score
+                    event = child
+            self.current_event = event
         return self.current_event
 
+    def compute_scores(self, event):
+        if self.current_event == ():
+            if event == (0, ):
+                score = self.hunger
+            else:
+                score = self.sleepiness
+        elif self.current_event == (0, 0):
+            if event == (0, 0, 0):
+                score = -self.world.food_stored
+            else:
+                score = 0
+        else:
+            index = self.world.animal_category.index(self.focus)
+            score = self.hunting_method[event][index]
+
+        return score
 
     def compute_status(self):
         t = self.event_tree
@@ -129,17 +174,14 @@ class Human:
     def go_to(self, location):
         raise NotImplementedError
 
+    def hunt(self):
+        print('hunt')
+
+    def do_it(self):
+        print('do_it')
+
     def choose_killing_method(self, animal_found):
         raise NotImplementedError
-
-    def get_hunt_success_probs(self, animal):
-        best_action = None
-        prob = 0
-        stab_prob = 0
-        shoot_prob = 0
-        throw_prob = 0
-        trap_prob = 0
-        return best_action, prob
 
     def trap(self, animal):
         raise NotImplementedError
