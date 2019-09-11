352ab0da9b745e1c7f88a57eba43bcb58bf7bfca
Shufan-Mao <smao9@illinois.edu>
2019-09-11 16:56:04 -0500

    initializing animals

--- src/animals.py
+++ src/animals.py
@@ -1,9 +1,16 @@
+import random
+
+
 class Animal:
 
     def __init__(self, world):
-        self.id_number = None
-        self.category = None
-        self.size = None
-        self.location = None
-        self.status = None
         self.world = world
+        self.id_number = len(self.world.animal_list)
+        self.category = random.choice(self.world.animal_category)
+        self.size = random.randint(self.world.animal_size[self.category][0], self.world.animal_size[self.category][1])
+        self.location = None
+
+
+
+
+
