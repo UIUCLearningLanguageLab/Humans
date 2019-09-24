25e7131b3c457e33acadcf81ecf2201ba5873917
Shufan-Mao <smao9@illinois.edu>
2019-09-24 17:55:06 -0500

    just test inner operations, without display

--- run_humans.py
+++ run_humans.py
@@ -5,7 +5,11 @@ def main():
     the_world = world.World()
     the_world.create_humans()
     the_world.create_animals()
-    the_display = display.Display(the_world)
-    the_display.root.mainloop()
+    for i in range(10):
+        print('running turn {}'.format(i))
+        the_world.next_turn()
+        print()
+    #the_display = display.Display(the_world)
+    #the_display.root.mainloop()
 
 main()
