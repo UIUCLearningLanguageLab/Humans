685d9af39e94b99b8a382f3edc1ec574ef2314aa
Shufan-Mao <smao9@illinois.edu>
2019-09-25 16:51:58 -0500

    testing the whole model

--- run_humans.py
+++ run_humans.py
@@ -1,14 +1,13 @@
 from src import world
+from src import config
 from src.display import display
 
 def main():
     the_world = world.World()
     the_world.create_humans()
     the_world.create_animals()
-    for i in range(10):
-        print('running turn {}'.format(i))
+    for i in range(config.World.num_turn):
         the_world.next_turn()
-        print()
     #the_display = display.Display(the_world)
     #the_display.root.mainloop()
 
