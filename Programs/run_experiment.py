32b9683706768e2d769b872e784a8197bc00244c
Shufan-Mao <smao9@illinois.edu>
2020-04-03 17:50:08 -0500

    Updated with parameter space

--- Programs/run_experiment.py
+++ Programs/run_experiment.py
@@ -6,13 +6,13 @@ num_run = 1
 s_task = True
 p_task = True
 models = ['cooc', 'sim', 'cooc_graph', 'sim_graph']
-parameters = ['window_size', 'window_weight', 'cooc_direction', 'normalization', 'reduction', 'sim_type']
+parameters = ['window_size', 'window_weight', 'window_type', 'normalization', 'reduction', 'sim_type']
 
-parameter_dict = {'window_size': [1,2,7],
-                   'window_weight': ['linear','flat'],
-                   'cooc_direction': ['forward', 'backward', 'summed', 'concatenated'],
+parameter_dict = {'window_size': [1],
+                   'window_weight': ['linear'],
+                   'window_type': ['forward','backward','summed'],  # , 'backward', 'summed', 'concatenated'],
                    'normalization': ['ppmi', 'non'],
-                   'reduction': ['svd', 'non'],
+                   'reduction': ['non','svd'],
                    'sim_type': ['cos', 'distance', 'corr']}
 
 
@@ -40,16 +40,16 @@ def run_experiment(num_run):
     corpora = []
     model_para_list = get_model_list(parameter_dict,parameters)
     output.output_model_dict(parameters, model_para_list)
-    num_model = len(model_para_list)
+    num_model = len(models) * len(model_para_list)
     corr_header = ['subject']
     corr_dict = {'subject':[]}
     ranking_header = ['subject','item']
     ranking_dict = {'subject':[],'item':[]}
     wb_header = ['subject']
     wb_dict = {'subject':[]}
-    corr_data_matrix = np.zeros(num_run, num_model)
-    wb_data_matrix = np.zeros(4*num_run, num_model)
-    ranking_data_matrix = np.zeros(1,num_model)
+    corr_data_matrix = np.zeros((num_run, num_model))
+    wb_data_matrix = np.zeros((4*num_run, num_model))
+    ranking_data_matrix = np.zeros((1, num_model))
 
     for i in range(num_run):
         # create a world and get world info
@@ -64,18 +64,18 @@ def run_experiment(num_run):
                 wb_dict['subject'].append(i+1)
         if s_task:
             t_verbs = kit['t_verbs']
-            pairs = kit['pairs']
             corr_dict['subject'].append(i+1)
             for verb in t_verbs:
                 for noun in p_nouns:
                     phrase = verb + '_' + noun
                     ranking_dict['item'].append(phrase)
                     ranking_dict['subject'].append(i+1)
-            standard_ranking = syntagmatic_task.get_standard_ranking(p_nouns, t_verbs, pairs)
+            standard_ranking = syntagmatic_task.get_standard_ranking(kit)
             current_ranking_matrix = standard_ranking.reshape(len(standard_ranking),1)
 
         # use world info to build 4 models according each model parameter
         for model_parameters in model_para_list:
+            print(model_parameters)
             id_parameters = model_para_list.index(model_parameters)
             word_bag, vocab_list, vocab_index_dict = build_models.corpus_transformation(linear_corpus)
             kit['vocab_list'] = vocab_list
@@ -86,17 +86,20 @@ def run_experiment(num_run):
 
             for model in models:
                 id_model = models.index(model) + 4 * id_parameters
-
                 if p_task:
-                    within_between = paradigmatic_task.run_task(kit,model)
+                    within_between = paradigmatic_task.run_task(kit,model)[1]
                     for j in range(4):
                         wb_data_matrix[4*i+j][id_model] = within_between[j]
+
                 if s_task:
                     model_corr, output_ranking = syntagmatic_task.run_task(kit,model)
+                    print(model_corr)
                     corr_data_matrix[i][id_model] = model_corr
-                    current_ranking_matrix = np.concatenate((ranking_data_matrix, output_ranking), 1)
+                    current_ranking_matrix = np.concatenate((current_ranking_matrix, output_ranking), 1)
+
 
         if s_task:
+            current_ranking_matrix = current_ranking_matrix[0:,1:]
             ranking_data_matrix = np.concatenate((ranking_data_matrix,current_ranking_matrix),0)
     if s_task:
         ranking_data_matrix = ranking_data_matrix[1:]
@@ -107,7 +110,6 @@ def run_experiment(num_run):
         output.output_exp(num_model, ranking_header, ranking_dict, 's_ranking.csv', ranking_data_matrix)
     if p_task:
         output.output_exp(num_model, wb_header, wb_dict, 'within_between.csv', wb_data_matrix)
-
 
 run_experiment(num_run)
 
